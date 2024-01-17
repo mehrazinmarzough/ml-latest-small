4f95a87bf0755b27797db17475db1596c0d52236
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2024-01-17 20:33:33 +0330

    user movie rating matrix created

--- ml-latest-small.py
+++ ml-latest-small.py
@@ -1,7 +1,16 @@
+import numpy as np
 import pandas as pd
+def CreateMatrix():
+    # Load data from CSV files
+    MoviesDf = pd.read_csv("movies.csv")
+    RatingsDf = pd.read_csv("ratings.csv")
 
-LinksDf = pd.read_csv("links.csv")
-MoviesDf = pd.read_csv("movies.csv")
-RatingsDf = pd.read_csv("ratings.csv")
-TagsDf = pd.read_csv("tags.csv")
+    user_movie_ratings = pd.merge(RatingsDf, MoviesDf, on='movieId')[['userId', 'movieId', 'rating']]
+    user_movie_ratings_pivot = user_movie_ratings.pivot_table(index='userId', columns='movieId', values='rating')
+    user_movie_ratings_matrix = user_movie_ratings_pivot.fillna(0).values
+
+    return user_movie_ratings_matrix
+
+
+UserMovieRating = CreateMatrix()
 
